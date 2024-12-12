4d90af9210aa99658374b818f13279393f847ac0
Yue Dong <ydong011@fiu.edu>
2024-12-12 14:17:09 -0600

    update

--- js/todolist/index.js
+++ js/todolist/index.js
@@ -7,8 +7,14 @@ const API = (() => {
 			response.json()
 		);
 
+	const deleteTodo = (id) =>
+		fetch([baseUrl, todoUrl, id].join("/")).then((response) =>
+			response.json()
+		);
+
 	return {
 		getTodos,
+		deleteTodo,
 	};
 })();
 
@@ -17,6 +23,7 @@ const View = (() => {
 	const domstr = {
 		inputBox: "todolist-input",
 		listContainer: "todolist-container",
+		deleteBtn: "delete-btn",
 	};
 
 	const createTmp = (todoArr) => {
@@ -24,8 +31,8 @@ const View = (() => {
 		todoArr.forEach((todo) => {
 			tmp += `
         	<li>
-						<span>${todo.title}</span>
-						<button>X</button>
+						<span>${todo.id}-${todo.title}</span>
+						<button class='delete-btn' id='${todo.id}'>X</button>
 					</li>
       `;
 		});
@@ -40,26 +47,63 @@ const View = (() => {
 })();
 
 // * ~~~~~~~~~~~~~~~~~~~ Model ~~~~~~~~~~~~~~~~~~~
-const Model = ((api) => {
+const Model = ((api, view) => {
+	class State {
+		#todolist = [];
+		#todoContainer = document.querySelector(
+			`.${view.domstr.listContainer}`
+		);
+		// querySelector: Nodelist vs. getElementById: HTMLCollection
+		// const todoContainer = document.getElementsByClassName(view.domstr.listContainer)[0];
+
+		get todolist() {
+			return this.#todolist;
+		}
+
+		set todolist(newtodolist) {
+			this.#todolist = [...newtodolist];
+
+			const tmp = view.createTmp(this.#todolist);
+			view.render(this.#todoContainer, tmp);
+		}
+	}
+
 	return {
 		...api,
+		State,
 	};
-})(API);
+})(API, View);
 
 // * ~~~~~~~~~~~~~~~~~~~ Controller ~~~~~~~~~~~~~~~~~~~
 const Controller = ((model, view) => {
-  // querySelector: Nodelist vs. getElementById: HTMLCollection
-  const todoContainer = document.querySelectorAll(`.${view.domstr.listContainer}`)[0];
-  // const todoContainer = document.getElementsByClassName(view.domstr.listContainer)[0];
-  
-	const init = () => {
-		model.getTodos().then((todolist) => {
-			const tmp = view.createTmp(todolist);
-      view.render(todoContainer, tmp);
+	const state = new model.State();
+	const todoContainer = document.querySelector(
+		`.${view.domstr.listContainer}`
+	);
+
+	const deleteTodo = () => {
+		todoContainer.addEventListener("click", (e) => {
+			if (e.target.className === view.domstr.deleteBtn) {
+				state.todolist = state.todolist.filter(
+					(todo) => +todo.id !== +e.target.id
+				);
+        model.deleteTodo(e.target.id);
+			}
 		});
 	};
 
-	return { init };
+	const init = () => {
+		model.getTodos().then((todolist) => {
+			state.todolist = todolist;
+		});
+	};
+
+  const bootstrap = () => {
+    init();
+    deleteTodo();
+  }
+
+	return { bootstrap };
 })(Model, View);
 
-Controller.init();
+Controller.bootstrap();
