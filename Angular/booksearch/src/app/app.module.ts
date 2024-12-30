24bb7ad43bb0a9129c52e8a86ec48e53176793d3
Yue Dong <ydong011@fiu.edu>
2024-12-30 14:48:11 -0600

    update

--- Angular/booksearch/src/app/app.module.ts
+++ Angular/booksearch/src/app/app.module.ts
@@ -3,16 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchComponent } from './components/search/search.component';
+import { BooklistComponent } from './components/booklist/booklist.component';
+import { BookItemComponent } from './components/book-item/book-item.component';
+import { WishListComponent } from './components/wish-list/wish-list.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    BooklistComponent,
+    BookItemComponent,
+    WishListComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ Angular/booksearch/src/app/services/book.service.ts
@@ -0,0 +1,39 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { BookRes, ExpectBook, ItemsEntity } from './interfaces/book.interface';
+import { map, Subject, tap } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class BookService {
+  baseUrl = 'https://www.googleapis.com/books/v1/volumes?q=';
+  bookSubject$ = new Subject<ExpectBook[]>();
+
+  constructor(private http: HttpClient) {}
+
+  getBooks(name: string) {
+    return this.http.get<BookRes>(this.baseUrl + name).pipe(
+      map((val: BookRes) => {
+        return val.items?.map(({ volumeInfo: info }: ItemsEntity) => {
+          return {
+            bookName: info.title,
+            bookPic: info.imageLinks.thumbnail,
+            publisher: info.publisher,
+            publishDate: info.publishedDate,
+            description: info.description,
+          } as ExpectBook;
+        });
+      }),
+      tap((val: ExpectBook[]) => {
+        this.bookSubject$.next(val);
+      })
+    );
+  }
+}
+
+// book pickture,
+// 	book name,
+// 	publisher,
+// 	publish date,
+// 	description.
